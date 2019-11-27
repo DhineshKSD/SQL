9fe5b1cf2142a1189e597e56f7a457eb834d8f68
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-11-27 19:37:49 +0530

    Update Weekly Sales Details.sql

--- Weekly Sales Details.sql
+++ Weekly Sales Details.sql
@@ -8,3 +8,13 @@
   order by Sold_Quantity desc
   
   
+SELECT week,product_name,Category_type,SUM(Sales) sales
+FROM
+(Select Product.product_name,Category.Category_type,Datepart(week,sale_date) week,sum(sale_quantity) Sales
+FROM Product
+  Join Sales on Product.Product_id=Sales.Product_id
+  Join Category on Product.Category_id = Category.Category_id
+GROUP BY  Product.product_name,Category.Category_type,sale_date
+ORDER BY  Product.product_name,Category.Category_type,week
+offset 0 rows) Sales
+GROUP BY week,product_name,Category_type
