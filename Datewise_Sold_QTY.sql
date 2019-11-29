8c01cc2c3d900440ba74128cf9817e4a0e6e58fd
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-11-29 17:54:08 +0530

    Update Datewise_Sold_QTY.sql

--- Datewise_Sold_QTY.sql
+++ Datewise_Sold_QTY.sql
@@ -14,5 +14,14 @@ having SUM(sale_quantity)= (select Max(qty )
    from (select SUM(sale_quantity)  qty 
      from Sales
      group by sales.sale_date ) tab)  ******/
+
+	 
+SELECT Product.Product_Name,Product.Product_id,Category.Category_type,SUM(Sales.sale_quantity) as Sold_Quantity
+FROM Product
+Join Sales on Product.Product_id=Sales.Product_id
+Join Category on Product.Category_id = Category.Category_id
+Group by Product.Product_Name,Product.Product_id,Category.Category_type
+order by Sold_Quantity desc
+  
   
   
